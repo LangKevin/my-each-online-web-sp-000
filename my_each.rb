cadc33d595ad446230e961ce9338fa49d940d931
kevin lang <langkevina@gmail.com>
2019-05-25 15:53:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  i = 0
+  while i < array.length
+    if block_given?
+      yield array[i]
+    else  
+      puts("Hey! No block was given!")
+      break
+  end
+  i = i + 1
+end
+array
 end
